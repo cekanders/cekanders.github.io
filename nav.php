507c4619038ddc9fccf05e439d3477c7a6255669
cekanders <66499842+cekanders@users.noreply.github.com>
2020-09-07 23:02:31 -0400

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,13 @@
+<nav>
+        <ul>
+            <div class="alignleft">
+                <li><a href="index.php" class= "<?php echo $index ?>" ><img src="images/chloe.png" alt="Logo" /></a></li>
+            </div>
+            <div class="alignright">
+                    <li><a href="me.php" class= "<?php echo $me ?>" >ABOUT</a></li>
+                    <li><a href="resume.php" class= "<?php echo $resume ?>" >RESUME</a></li>
+            </div>
+        </ul>
+</nav>
+
+<div style="clear: both;"></div>
