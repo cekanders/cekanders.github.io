826ead6fa47cbe5513f13355cd12234776a7d607
cekanders <66499842+cekanders@users.noreply.github.com>
2020-09-08 21:16:55 -0400

    Update resume.php

--- resume.php
+++ resume.php
@@ -1,4 +1,8 @@
-<?php include("includes/head.php"); ?>
-<?php include("includes/nav.php"); ?>
-
-<iframe src="/uploads/images/media/Kanders_Chloe_2022.pdf" width="100%" height="800px"></iframe>
+<?php include("includes/init.php"); 
+include("includes/head.php"); 
+$resume = "current_page";
+$me = "other";
+$index = "other";
+include("includes/nav.php"); ?>
+<div class='row2'></div>
+<iframe src="/uploads/images/media/Kanders_Chloe.pdf" width="100%" height="800px"></iframe>
