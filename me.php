ddc505a9108b10c2fdc2ec0c4e5db207d5d0e875
cekanders <66499842+cekanders@users.noreply.github.com>
2020-09-08 23:18:40 -0400

    Update me.php

--- me.php
+++ me.php
@@ -8,7 +8,7 @@ include("includes/nav.php"); ?>
 <div class='row2'>
     <p><span style="font-family: Avenir Heavy; font-size: 40px">Hey! I’m Chloe.</span><br></br>
     I’m a junior at <a href='https://www.cornell.edu/'>Cornell University</a> majoring in Information Science with a concentration in User Experience, minoring in Business. 
-    <br></br>I am currently working as a Product Management Intern for <a href='https://www.simplifed.us/'>SimpliFed</a>, an virtual lactation consulting service. 
+    <br></br>I am currently working as a Product Management Intern for <a href='https://www.simplifed.us/'>SimpliFed</a>, a virtual lactation consulting service. 
     This past summer, I worked as a Product Design Intern at <a href='https://www.catch-nyc.com/'>Catch New York</a>. On campus, I am involved with the <a href="https://milstein-program.as.cornell.edu/">Milstein Program in Technology and Humanity</a> as well as the Society for Women in Business.</p>
     
     <img src="images/me2.png" alt="me"/>
