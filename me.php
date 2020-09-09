6158c63435bcc2894bb5e34720df9acb8cda5bcb
cekanders <66499842+cekanders@users.noreply.github.com>
2020-09-08 20:10:30 -0400

    Update me.php

--- me.php
+++ me.php
@@ -1,12 +1,18 @@
-<?php include("includes/head.php"); ?>
-<?php include("includes/nav.php"); ?>
+<?php include("includes/init.php"); 
+include("includes/head.php"); 
+$me = "current_page";
+$resume = "other";
+$index = "other";
+include("includes/nav.php"); ?>
 
 <div class='row2'>
     <p><span style="font-family: Avenir Heavy; font-size: 40px">Hey! I’m Chloe.</span><br></br>
     I’m a junior at <a href='https://www.cornell.edu/'>Cornell University</a> majoring in Information Science with a concentration in User Experience, minoring in Business. 
-    <br></br>I am currently working as a Product Management Intern for <a href='https://www.simplifed.edu/'>SimpliFed</a>, an virtual lactation consulting service. 
+    <br></br>I am currently working as a Product Management Intern for <a href='https://www.simplifed.us/'>SimpliFed</a>, an virtual lactation consulting service. 
     This past summer, I worked as a Product Design Intern at <a href='https://www.catch-nyc.com/'>Catch New York</a>. On campus, I am involved with the <a href="https://milstein-program.as.cornell.edu/">Milstein Program in Technology and Humanity</a> as well as the Society for Women in Business.</p>
+    
     <img src="images/me2.png" alt="me"/>
+    
 </div>
 
 <p>I am passionate about solving problems to create meaningful change for individuals and organizations through digital platforms. In my free time, I enjoy running, spending time with my friends and family, trying new restaurants in Ithaca and traveling.
