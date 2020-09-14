e7a973b2edcc07c709e700cf97aa03f65089e4dd
cekanders <66499842+cekanders@users.noreply.github.com>
2020-09-14 14:55:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,32 @@ include("includes/nav.php"); ?>
     <p>I design <span style="font-family: Avenir Heavy">data-driven solutions</span> and <span style="font-family: Avenir Heavy">product-minded strategies</span> in the digital marketing and adtech space. Read about my work below!</p>
 </div>
 
+<div class="tag">
+    <p>CASE STUDIES</p>
+</div>
+
+<div class='space'></div>
+
+<div class='row'>
+
+    <div class='first'>
+        <figure>
+        <a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256" target="_blank">
+        <img src="uploads/images/2.png" alt="SoundCloud" />
+        </a>
+        </figure>
+    </div>
+
+    <div class='second'>
+        COURSEWORK
+        <div class='newtag'><p>SoundCloud</p></div>
+        <p>UX case study exploring user interactions through comments on SoundCloud.</p>
+        <div class='space'></div>
+        <div class='button'><a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256" target="_blank"><p>READ MORE</p></a></div>
+    </div>
+</div>
+
+<hr>
 
 <div class="tag">
     <p>PROJECTS</p>
@@ -68,33 +94,6 @@ include("includes/nav.php"); ?>
         <p>Health and wellness space based in NYC website, blog, and virtual community design.</p>
         <div class='space'></div>
         <div class='button'><a href="sageandsound.php"><p>READ MORE</p></a></div>
-    </div>
-</div>
-
-<hr>
-
-<div class="tag">
-    <p>CASE STUDIES</p>
-</div>
-
-<div class='space'></div>
-
-<div class='row'>
-
-    <div class='first'>
-        <figure>
-        <a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256">
-        <img src="uploads/images/2.png" alt="SoundCloud" />
-        </a>
-        </figure>
-    </div>
-
-    <div class='second'>
-        COURSEWORK
-        <div class='newtag'><p>SoundCloud</p></div>
-        <p>UX case study exploring user interactions through comments on SoundCloud.</p>
-        <div class='space'></div>
-        <div class='button'><a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256"><p>READ MORE</p></a></div>
     </div>
 </div>
 
