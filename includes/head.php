131b7f27f10748f7cdb5f60f67ad689e8b8c4c50
cekanders <66499842+cekanders@users.noreply.github.com>
2020-09-14 17:37:19 -0400

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -6,6 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Chloe Kanders' Portfolio</title>
   <link rel="stylesheet" type="text/css" href="styles/theme.css" media="all" />
-  <link rel="icon" href="images/logo3.png">
+  <link rel="icon" href="images/logo3.png"/>
 </head>
 <body>
