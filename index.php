a2e3ecbf76b2ae256e8c33d149150b1de3762b93
cekanders <66499842+cekanders@users.noreply.github.com>
2020-09-14 11:45:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,101 @@
-<?php include("includes/init.php"); ?> 
-<?php include("includes/head.php"); ?>
-<?php include("includes/nav.php"); ?>
+<?php include("includes/init.php"); 
+include("includes/head.php"); 
+include("includes/nav.php"); ?>
 
 <div class='intro'>
     <p>👋 Hello, I'm Chloe.<p>
 </div>
 
 <div class='intro2'>
-    <p>I design <span style="font-family: Avenir Heavy">data-driven solutions</span> and <span style="font-family: Avenir Heavy">product-minded strategies</span> in the digital marketing and adtech space. Check out some of my work below!</p>
+    <p>I design <span style="font-family: Avenir Heavy">data-driven solutions</span> and <span style="font-family: Avenir Heavy">product-minded strategies</span> in the digital marketing and adtech space. Read about my work below!</p>
 </div>
 
-<div class="gallery">
 
-<div>
-    <figure>
-        <a href="parachute.php">
-        <img src="uploads/images/1.png" alt="Parachute"/>
-        <img src="uploads/images/5.png" alt="Parachute"/>
+<div class="tag">
+    <p>PROJECTS</p>
+</div>
+
+<div class='space'></div>
+
+<div class='row'>
+    <div class='first'>
+        <figure>
+            <a href="parachute.php">
+            <img src="uploads/images/1.png" alt="Parachute"/>
+            </a>
+        </figure>
+    </div>
+
+    <div class='second'>
+        INTERNSHIP
+        <div class='newtag'><p>Parachute Digital Solutions</p></div>
+        <p>Email campaign promoting an exclusive insurance offer for  eligible organization members.</p>
+        <div class='space'></div>
+        <div class='button'><a href="parachute.php"><p>READ MORE</p></a></div>
+    </div>
+</div>
+
+<div class='row'>
+    <div class='first'>
+        <figure>
+            <a href="bluefoundry.php">
+            <img src="uploads/images/3.png" alt="Blue Foundry" />
+            </a>
+        </figure>
+    </div>
+
+    <div class='second'>
+        INTERNSHIP
+        <div class='newtag'><p>Blue Foundry Bank</p></div>
+        <p>Onboarding email series for digital banking products. </p>
+        <div class='space'></div>
+        <div class='button'><a href="bluefoundry.php"><p>READ MORE</p></a></div>
+    </div>
+</div>
+
+<div class='row'>
+    <div class='first'>
+        <figure>
+        <a href="sageandsound.php">
+        <img src="uploads/images/4.png" alt="Sage and Sound" />
         </a>
-    </figure>
+        </figure>
+    </div>
 
-    <figure>
-        <a href="bluefoundry.php">
-        <img src="uploads/images/3.png" alt="Blue Foundry" />
-        <img src="uploads/images/7.png" alt="Blue Foundry" />
+    <div class='second'>
+        INTERNSHIP
+        <div class='newtag'><p>Sage and Sound</p></div>
+        <p>Health and wellness space based in NYC website, blog, and virtual community design.</p>
+        <div class='space'></div>
+        <div class='button'><a href="sageandsound.php"><p>READ MORE</p></a></div>
+    </div>
+</div>
+
+<hr>
+
+<div class="tag">
+    <p>CASE STUDIES</p>
+</div>
+
+<div class='space'></div>
+
+<div class='row'>
+
+    <div class='first'>
+        <figure>
+        <a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256">
+        <img src="uploads/images/2.png" alt="SoundCloud" />
         </a>
-    </figure>
+        </figure>
+    </div>
+
+    <div class='second'>
+        COURSEWORK
+        <div class='newtag'><p>SoundCloud</p></div>
+        <p>UX case study exploring user interactions through comments on SoundCloud.</p>
+        <div class='space'></div>
+        <div class='button'><a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256"><p>READ MORE</p></a></div>
+    </div>
 </div>
-
-<div>
-  <figure>
-      <a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256">
-      <img src="uploads/images/2.png" alt="SoundCloud" />
-      <img src="uploads/images/6.png" alt="SoundCloud" />
-      </a>
-    </figure>
-
-    <figure>
-      <a href="sageandsound.php">
-      <img src="uploads/images/4.png" alt="Sage and Sound" />
-      <img src="uploads/images/8.png" alt="Sage and Sound" />
-      </a>
-    </figure>
-</div>
-
-
 
 </body>
