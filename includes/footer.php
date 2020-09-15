4d0eb9e758f53baf16c6b1a13b4b08c48b25d301
cekanders <66499842+cekanders@users.noreply.github.com>
2020-09-14 23:01:53 -0400

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<div class='space'></div>
+<hr>
+<div class='space'></div>
+<div class='center2'>
+    <div class='pushleft'>
+        <a href='https://www.linkedin.com/in/chloe-kanders-6b390a184/'><img src='images/logo3.png' alt='logo'></a>
+    </div>
+    This website was created by Chloe Kanders, Copyright 2020.
+</div>
+<div class='space'></div>
+</body>
