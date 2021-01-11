b9b1a25ee838d38f0adf5ce0a64602b9ea61494c
cekanders <66499842+cekanders@users.noreply.github.com>
2021-01-11 10:54:05 -0500

    Update me.php

--- me.php
+++ me.php
@@ -7,15 +7,22 @@ include("includes/nav.php"); ?>
 
 <div class='row2'>
     <p><span style="font-family: Avenir Heavy; font-size: 40px">Hey! I’m Chloe.</span><br></br>
-    I’m a junior at <a href='https://www.cornell.edu/'>Cornell University</a> majoring in Information Science with a concentration in User Experience, minoring in Business. 
-    <br></br>I am currently working as a Product Management Intern for <a href='https://www.simplifed.us/'>SimpliFed</a>, a virtual lactation consulting service. 
-    This past summer, I worked as a Product Design Intern at <a href='https://www.catch-nyc.com/'>Catch New York</a>. On campus, I am involved with the <a href="https://milstein-program.as.cornell.edu/">Milstein Program in Technology and Humanity</a> as well as the Society for Women in Business.</p>
+    
+    I’m a junior at <a href='https://www.cornell.edu/'>Cornell University</a> studying Information Science, with concentrations in User Experience and Data Science, and Business. 
+    <br></br>I am passionate about solving problems to create meaningful change for individuals and organizations via digital platforms. 
+    On campus, I am involved with <a href='https://www.cornellappdev.com/'>AppDev</a>, the <a href="https://milstein-program.as.cornell.edu/">Milstein Program in Technology and Humanity</a>, 
+    and the <a href='https://www.cornellswib.com/'>Society for Women in Business</a>.
+    In my free time, I enjoy exercising, listening to music, trying new restaurants in Ithaca, and traveling.</p>
     
     <img src="images/me2.png" alt="me"/>
     
 </div>
 
-<p>I am passionate about solving problems to create meaningful change for individuals and organizations through digital platforms. In my free time, I enjoy running, spending time with my friends and family, trying new restaurants in Ithaca, and traveling.
-<br></br>If you would like to contact me, feel free to send me an email at cek223@cornell.edu or add me on <a href='https://www.linkedin.com/in/chloe-kanders-6b390a184/'>LinkedIn</a>.</p>
-
+<div class='linked'>
+<p> My journey into product began with my internship at <a href='https://www.catch-nyc.com/'>Catch New York</a> last summer. 
+    I found myself fascinated by understanding the consumer and creating products and campaigns that align with the needs of the target audience in a variety of industries.
+    I have continued fostering my passion for strategizing and defining product vision by working with <a href='https://simplifed.us/'>SimpliFed</a>, 
+        a startup in the healthcare space, this past semester.
+<br></br>I love to talking to new people and learning about their projects and experiences. If you would like to contact me, feel free to send me an email at cek223@cornell.edu or add me on <a href='https://www.linkedin.com/in/chloe-kanders-6b390a184/'>LinkedIn</a>.</p>
+</div>
 <?php include("includes/footer.php"); ?>
