eb2ad1790fb21bac765262e00fb40d78b46a8d9f
cekanders <66499842+cekanders@users.noreply.github.com>
2021-01-12 16:49:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,61 +1,35 @@
 <?php include("includes/init.php"); 
 include("includes/head.php"); 
+$me = "other";
+$resume = "other";
+$work = "current_page";
 include("includes/nav.php"); ?>
 
 <div class='intro'>
     <p>👋 Hello, I'm Chloe.<p>
 </div>
-
 <div class='intro2'>
-    <p>I design <span style="font-family: Avenir Heavy">data-driven solutions</span> and <span style="font-family: Avenir Heavy">product-minded strategies</span> in the digital marketing and adtech space. Read about my work below!</p>
+    <p>I create data-driven solutions and product-minded strategies.</p>
 </div>
 
-<div class="tag">
-    <p>CASE STUDIES</p>
-</div>
 
 <div class='space'></div>
-
-<div class='row'>
-
-    <div class='first'>
-        <figure>
-        <a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256" target="_blank">
-        <img src="uploads/images/2.png" alt="SoundCloud" />
-        </a>
-        </figure>
-    </div>
-
-    <div class='second'>
-        <div class='small'><p>COURSEWORK</p></div>
-        <div class='newtag'><p>SoundCloud</p></div>
-        <p>UX case study exploring user interactions through comments on SoundCloud.</p>
-        <div class='space'></div>
-        <div class='button'><a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256" target="_blank"><p>READ MORE</p></a></div>
-    </div>
-</div>
-
-<hr>
-
-<div class="tag">
-    <p>PROJECTS</p>
-</div>
-
 <div class='space'></div>
 
 <div class='row'>
     <div class='first'>
         <figure>
             <a href="parachute.php">
-            <img src="uploads/images/1.png" alt="Parachute"/>
+            <img class='img_home' src="uploads/images/1.png" alt="Parachute"/>
             </a>
         </figure>
     </div>
 
     <div class='second'>
-        <div class='small'><p>INTERNSHIP</p></div>
+        <div class='small'><p>UX INTERNSHIP</p></div>
         <div class='newtag'><p>Parachute Digital Solutions</p></div>
-        <p>Email campaign promoting an exclusive insurance offer for  eligible organization members.</p>
+        <p>Email campaign promoting an exclusive insurance offer for  eligible organization members</p>
+        <div class='space'></div>
         <div class='space'></div>
         <div class='button'><a href="parachute.php"><p>READ MORE</p></a></div>
     </div>
@@ -64,18 +38,41 @@ include("includes/nav.php"); ?>
 <div class='row'>
     <div class='first'>
         <figure>
-            <a href="bluefoundry.php">
-            <img src="uploads/images/3.png" alt="Blue Foundry" />
+            <a href="simplifed.php">
+            <img class='img_home' src="uploads/images/2.png" alt="SimpliFed"/>
             </a>
         </figure>
     </div>
 
     <div class='second'>
-        <div class='small'><p>INTERNSHIP</p></div>
-        <div class='newtag'><p>Blue Foundry Bank</p></div>
-        <p>Onboarding email series for digital banking products. </p>
+        <div class='small'><p>PM INTERNSHIP</p></div>
+        <div class='newtag'><p>SimpliFed</p></div>
+        <p>Product management for a virtual lactaction consulting service</p>
         <div class='space'></div>
-        <div class='button'><a href="bluefoundry.php"><p>READ MORE</p></a></div>
+        <div class='space'></div>
+        <div class='button'><a href="simplifed.php"><p>READ MORE</p></a></div>
+    </div>
+</div>
+
+
+
+<div class='row'>
+
+    <div class='first'>
+        <figure>
+        <a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256" target="_blank">
+        <img class='img_home' src="uploads/images/3.png" alt="SoundCloud" />
+        </a>
+        </figure>
+    </div>
+
+    <div class='second'>
+        <div class='small'><p>CASE STUDY</p></div>
+        <div class='newtag'><p>SoundCloud</p></div>
+        <p>UX case study exploring user interactions through comments</p>
+        <div class='space'></div>
+        <div class='space'></div>
+        <div class='button'><a href="https://medium.com/@cek223/soundcloud-concept-diving-deeper-into-discussion-d8cb78cff256" target="_blank"><p>VIEW CASE STUDY</p></a></div>
     </div>
 </div>
 
@@ -83,17 +80,37 @@ include("includes/nav.php"); ?>
     <div class='first'>
         <figure>
         <a href="sageandsound.php">
-        <img src="uploads/images/4.png" alt="Sage and Sound" />
+        <img class='img_home' src="uploads/images/4.png" alt="Sage and Sound" />
         </a>
         </figure>
     </div>
 
     <div class='second'>
-        <div class='small'><p>INTERNSHIP</p></div>
+        <div class='small'><p>UX INTERNSHIP</p></div>
         <div class='newtag'><p>Sage and Sound</p></div>
-        <p>Health and wellness space based in NYC website, blog, and virtual community design.</p>
+        <p>Design for health and wellness space website, blog, and virtual community</p>
+        <div class='space'></div>
         <div class='space'></div>
         <div class='button'><a href="sageandsound.php"><p>READ MORE</p></a></div>
+    </div>
+</div>
+
+<div class='row'>
+    <div class='first'>
+        <figure>
+            <a href="bluefoundry.php">
+            <img class='img_home' src="uploads/images/5.png" alt="Blue Foundry" />
+            </a>
+        </figure>
+    </div>
+
+    <div class='second'>
+        <div class='small'><p>UX INTERNSHIP</p></div>
+        <div class='newtag'><p>Blue Foundry Bank</p></div>
+        <p>Onboarding email series for digital banking products </p>
+        <div class='space'></div>
+        <div class='space'></div>
+        <div class='button'><a href="bluefoundry.php"><p>READ MORE</p></a></div>
     </div>
 </div>
 
