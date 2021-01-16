debf1e7607a2f561c5f5e035614a6bd3b8c233b7
cekanders <66499842+cekanders@users.noreply.github.com>
2021-01-16 16:54:43 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <div class='space'></div>
 <div class='center2'>
     <div class='pushleft'>
-        <img class='img_footer' src='images/logo3.png' alt='logo'>
+        <img class='img_footer' src='images/logo3.png' alt='logo'/>
     </div>
     <p>This website was created by Chloe Kanders, Copyright 2020.</p>
 </div>
